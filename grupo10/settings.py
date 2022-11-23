29ebf28b53ede76eea4f68fc2508dcd7ef0a60db
diego-dotcom <mendizabal.diego@gmail.com>
2022-11-23 20:04:11 -0300

    Se cambió el template

--- grupo10/settings.py
+++ grupo10/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cwovl58_r3u0bz33lxr0%nl^y332l3!=a6zmkpg9ambm#g-dlp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_todolist', 
         'USER': 'postgres', 
-        'PASSWORD': 'Hermes84',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
